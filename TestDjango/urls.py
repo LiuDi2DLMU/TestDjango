bca404dbf22c29b3acf49f2364c3eff45682f915
liu xing <644109839@qq.com>
2022-11-14 16:56:59 +0800

    add "first" page

--- TestDjango/urls.py
+++ TestDjango/urls.py
@@ -18,5 +18,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.hello),
+    url("hello/", views.hello),
+    url("first/", views.first),
 ]
--- TestDjango/views.py
+++ TestDjango/views.py
@@ -1,5 +1,12 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def hello(request):
     return HttpResponse("Hello world ! ")
+
+
+def first(request):
+    context          = {}
+    context['hello'] = 'Hello World, first!'
+    return render(request, 'first.html', context)
